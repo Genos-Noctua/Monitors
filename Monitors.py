89f80add5d968f2d8533436f25cf823f4a990ac2
Genos-Noctua <106928018+Genos-Noctua@users.noreply.github.com>
2023-05-27 00:16:51 +0300

    Update Monitors.py

--- Monitors.py
+++ Monitors.py
@@ -1,4 +1,4 @@
-#Monitors 2.0
+#Monitors 2.1
 import os, time
 from PIL import Image
 from pystray import Icon, Menu as menu, MenuItem as item
@@ -13,10 +13,12 @@ def switch(mode):
         os.system('.\MultiMonitorTool.exe /disable \\\\.\\DISPLAY3')
         os.system('.\MultiMonitorTool.exe /enable \\\\.\\DISPLAY2')
         os.system('.\MultiMonitorTool.exe /LoadConfig "2configs"')
-    else:
+    elif mode == "3":
         for x in range(3):
             os.system('.\MultiMonitorTool.exe /LoadConfig "3configs"')
             time.sleep(0.1)
+    elif mode == "TV":
+        os.system('.\MultiMonitorTool.exe /LoadConfig "TVconfigs"')
     f = open("state", "w+")
     print(mode, file=f, end="", flush=True)
     f.close()
@@ -28,7 +30,7 @@ def main():
         mode = file.read()
         switch(mode)
     with Image.open("Monitor.ico") as f:
-        icon = Icon("Monitors", f, menu=menu(item("1", lambda: switch("1")), item("2", lambda: switch("2")), item("3", lambda: switch("3"))))
+        icon = Icon("Monitors", f, menu=menu(item("1", lambda: switch("1")), item("2", lambda: switch("2")), item("3", lambda: switch("3")), item("TV", lambda: switch("TV"))))
         icon.run()
 
 if __name__ == "__main__":
